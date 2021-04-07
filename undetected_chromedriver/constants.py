bee715dc972bfbef54b897a144b8579528bd424d
Aleix Rodríguez Alameda <aleixrodriala@gmail.com>
2021-04-07 14:41:48 +0200

    Webgl Vendor

--- undetected_chromedriver/constants.py
+++ undetected_chromedriver/constants.py
@@ -1,1 +1,3 @@
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
+VENDOR = 'Google Inc.'
+RENDERER = 'ANGLE (NVIDIA GeForce GTX 1650 Direct3D11 vs_5_0 ps_5_0)'
